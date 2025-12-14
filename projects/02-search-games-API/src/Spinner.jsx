a4f91d1c9b3430fd40c52d1bb06cd23cd019e496
Lautaro Martinez <laumartinez.exe@gmail.com>
2025-12-13 22:30:55 -0300

    Refactor Spinner component and CSS class names for consistency

--- projects/02-search-games-API/src/Spinner.jsx
+++ projects/02-search-games-API/src/Spinner.jsx
@@ -1,8 +1,9 @@
 const Spinner = ({ message = ''}) => {
     return (
-        <div className="loading">
-            <img src="/src/assets/spinner-double.svg" alt="spinner" className="loading__spinner"/>
-        <p>Loading{message ? ` ${message}...` : '...'}</p>
+        <div className="spinner">
+            <img className="spinner__icon"
+                src="/src/assets/spinner-double.svg" alt="spinner" />
+            <p>Loading{message ? ` ${message}...` : '...'}</p>
         </div>
     )
 }
